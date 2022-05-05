f70e4b4ba96c50fa0de4956e4fcba68b54ce8439
afonime royal <afonimeroyal@gmail.com>
2022-05-05 01:19:25 +0100

    Debugging

--- src/views/About.vue
+++ src/views/About.vue
@@ -63,7 +63,7 @@
           <div class="col-lg-6 " data-aos="fade-left"  data-aos-delay="300">
 
             <div class="diffprizes mb-4 pb-4">
-              <i class="fa fa-user fa-3x m5-3 pe-4"></i>
+              <i class="fa fa-user fa-3x m5-3 pe-4 " style="color:#21D192"></i>
               <div class="text">
               <h3><b>Manager of the week</b></h3>
               <p>This award goes to the manager with the highest points for a particluar weekday</p>
@@ -72,7 +72,7 @@
             </div>
 
             <div class="diffprizes">
-              <i class="fa fa-female fa-3x m5-3 pe-4"></i>
+              <i class="fas fa-brain fa-3x m5-3 pe-4" style="color:#21D192"></i>
               <div class="text">
               <h3><b> Manager of the month</b></h3>
               <p>This award goes to the manager with the highest points for a month</p>
@@ -87,7 +87,8 @@
           <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-right"  data-aos-delay="300">
 
             <div class="diffprizes mb-4 pb-4">
-              <i class="fa fa-bolt fa-3x m5-3 pe-4"></i>
+            
+              <i class="fa fa-bolt fa-3x m5-3 pe-4" style="color:#21D192"></i>
               <div class="text">
               <h3><b>Female manager of the week</b></h3>
               <p>This award goes to the female manager with the highest points for a particluar weekday</p>
@@ -96,7 +97,7 @@
             </div>
 
             <div class="diffprizes">
-              <i class="fa fa-female fa-3x m5-3 pe-4"></i>
+              <i class="fa fa-female fa-3x m5-3 pe-4" style="color:#21D192"></i>
               <div class="text">
               <h3><b>Female manager of the month</b></h3>
               <p>This award goes to the female manager with the highest points for a month</p>
